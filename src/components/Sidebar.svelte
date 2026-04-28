979c076f741e87f03f057533ff6d372c740569c3
Lauti <lautarosaintmartin2@gmail.com>
2026-04-27 22:49:47 -0300

    Add layout and sidebar components, update routing and configuration

---
+++ src/components/Sidebar.svelte
@@ -0,0 +1,16 @@
+
+<script lang="ts">
+    import { ROUTES } from "@core/utils/router";
+    
+    let { activePage } = $props();
+
+
+</script>
+
+<aside>
+    {#each ROUTES as route}
+
+    <p class="text-white text-2xl">{route.name}</p>
+
+    {/each}
+</aside>
---
+++ src/core/utils/router.ts
@@ -0,0 +1,12 @@
+export const ROUTES = [
+    {
+        path: "/",
+        name: "Panel",
+        public: false,
+    },
+    {
+        path: "/auth/login",
+        name: "Iniciar Sesion",
+        public: true,
+    },
+]
