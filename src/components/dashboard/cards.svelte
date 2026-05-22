c70150add80ed525da2ff42ddd09856197b69d38
Lauti <lautarosaintmartin2@gmail.com>
2026-05-21 21:13:40 -0300

    feat: add Cards component to Dashboard and adjust heading styling

---
+++ src/components/dashboard/cards.svelte
@@ -0,0 +1,11 @@
+<div class="w-64 block rounded-lg bg-slate-800 shadow-lg dark:bg-white flex-col">
+    <a href="/./users">
+        <img class="rounded-t-lg w-auto h-auto" src="https://imgs.search.brave.com/xcOVRrAgskQMng5oELyxGF8wKmX1gl9Rvz3a2jbaV3A/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9zdGF0/aWMudmVjdGVlenku/Y29tL3N5c3RlbS9y/ZXNvdXJjZXMvdGh1/bWJuYWlscy8wMzAv/MjE2LzYyNS9zbWFs/bC91c2VyLWFjY291/bnQtaGVscC1pY29u/LWRlc2lnbi12ZWN0/b3IuanBn" alt="" />
+    </a>
+        <div class="p-6 rounded-md bg-slate-800 dark:bg-white text-white dark:text-black">
+            <h5 class="mb-2 text-xl font-medium leading-tight">Usuarios</h5>
+                <a href="/users" class="inline-block rounded bg-blue-600 px-6 py-2 text-xs font-medium uppercase text-white hover:bg-blue-700">
+                    Usuarios
+                </a>
+        </div>
+</div>
